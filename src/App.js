4671d7dbee9265aadb746e9490d34fc2dcca75a4
murraymack <murraymack@gmail.com>
2020-11-10 13:26:45 -0700

    Update quick brown fox

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          The quick brown fox jumps over the lazy dog.
         </p>
         <a
           className="App-link"
