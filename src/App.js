e61c1fc5915ffa543b9a7fb8d8ed620ac571f01e
murraymack <murraymack@gmail.com>
2020-11-10 20:49:35 -0700

    849pm

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports';
+Amplify.configure(awsconfig);
 
 function App() {
   return (
